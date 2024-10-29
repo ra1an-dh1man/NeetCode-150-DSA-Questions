de501fde3eee7c8bece7c1fe6932f9a767d52160
Rajan Dhiman <dhimanrajan7252@gmail.com>
2024-10-29 15:26:27 +0530

    Create ContainsDuplicate.java

---
+++ Arrays & Hashing/ContainsDuplicate.java
@@ -0,0 +1,10 @@
+class  ContainsDuplicate{
+    public boolean containsDuplicate(int[] nums) {
+        Set<Integer> a=new HashSet<>();
+        for(int n:nums){
+            if(a.contains(n))return true;
+            else a.add(n);
+        }
+        return false;
+    }
+}
