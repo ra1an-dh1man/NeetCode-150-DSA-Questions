000a87db14a9176083c517fa79e77d90c4814dff
Rajan Dhiman <dhimanrajan7252@gmail.com>
2024-10-29 15:29:01 +0530

    Create TwoSum.java

---
+++ Arrays & Hashing/TwoSum.java
@@ -0,0 +1,14 @@
+class TwoSum {
+    public int[] twoSum(int[] nums, int target) {
+        int[] result = new int[2];
+        Map<Integer,Integer> map = new HashMap<>();
+        for(int i=0; i<nums.length ; i++){
+            int complement = target - nums[i];
+            if(map.containsKey(complement)){
+                return new int[]{i,map.get(complement)};
+            }
+            map.put(nums[i],i);
+        }
+        return result;
+    }
+}
