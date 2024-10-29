0112ee710c9678db85144335fdcce1901de43c01
Rajan Dhiman <dhimanrajan7252@gmail.com>
2024-10-29 15:28:23 +0530

    Create ValidAnagram.java

---
+++ Arrays & Hashing/ValidAnagram.java
@@ -0,0 +1,21 @@
+class ValidAnagram {
+    public boolean isAnagram(String s, String t) {
+    if (s.length() == t.length()) {
+                    char[] arr1 = s.toCharArray();
+                    char[] arr2 = t.toCharArray();
+                    Arrays.sort(arr1);
+                    Arrays.sort(arr2);
+                    if(Arrays.equals(arr1,arr2)){
+                        return true;
+                       
+                    }
+                    else
+                        return false;
+
+                    
+                }
+                else 
+                return false;
+          
+        }
+}
